09b0c18439d51ccd1cf1a5754b916240e94d0c74
Dimokus <Dimokus@mail.ru>
2023-04-01 13:30:05 +0300

    Update main_cosmossdk.sh

--- deploy/main_cosmossdk.sh
+++ deploy/main_cosmossdk.sh
@@ -4,7 +4,12 @@
 #!/bin/bash
 # Часть 1 Установка ПО
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+if [[ -z $BINARY_LINK ]]
+then
 apt install -y nano tar wget lz4 zip jq runit build-essential git make gcc nvme-cli
+else
+apt install -y nano tar wget lz4 zip jq runit
+fi
 runsvdir -P /etc/service &
 if [[ -z $GO_VERSION ]]
 then
