691a04c397a601e27f025288eccabc4044e430a7
Dimokus <Dimokus@mail.ru>
2023-05-02 20:01:49 +0300

    Update main_cosmossdk.sh

--- deploy/main_cosmossdk.sh
+++ deploy/main_cosmossdk.sh
@@ -4,7 +4,7 @@
 #!/bin/bash
 # Часть 1 Установка ПО
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt install -y nano tar wget lz4 zip jq runit build-essential git make gcc nvme-cli pv golang-go unzip
+apt install -y nano tar wget lz4 zip jq runit build-essential git make gcc nvme-cli pv unzip
 runsvdir -P /etc/service &
 if [[ -z $GO_VERSION ]]
 then
