092efa310190733e16d90597429c71248410a8b9
Dimokus <Dimokus@mail.ru>
2023-05-30 14:09:44 +0300

    Update main_cosmossdk.sh

--- deploy/main_cosmossdk.sh
+++ deploy/main_cosmossdk.sh
@@ -6,89 +6,39 @@
 TZ=Europe/Kiev && ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt install -y nano tar wget lz4 zip jq runit build-essential git make gcc nvme-cli pv unzip
 runsvdir -P /etc/service &
-if [[ -z $GO_VERSION ]]
-then
-GO_VERSION="1.20.1"
-fi
+if [[ -z $GO_VERSION ]]; then GO_VERSION="1.20.1"; fi
 wget https://go.dev/dl/go$GO_VERSION.linux-amd64.tar.gz && tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
-PATH=$PATH:/usr/local/go/bin
-echo $PATH
-go version
-sleep 2
-echo 'export PATH='$PATH:/usr/local/go/bin >> /root/.bashrc
-if [[ -n $SSH_PASS ]] # Если задана переменная SSH_PASS - включаем службу SSH
-then 
-apt install -y ssh
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && (echo ${SSH_PASS}; echo ${SSH_PASS}) | passwd root && service ssh restart
-fi
-if [[ -n $SSH_KEY ]]
-then
-apt install -y ssh
-echo $SSH_KEY > /root/.ssh/authorized_keys
-chmod 0600 /root/.ssh/authorized_keys
-service ssh restart
-fi
-
+PATH=$PATH:/usr/local/go/bin && go version && echo 'export PATH='$PATH:/usr/local/go/bin >> /root/.bashrc
+if [[ -n $SSH_PASS ]] ; then apt install -y ssh; echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && (echo ${SSH_PASS}; echo ${SSH_PASS}) | passwd root && service ssh restart; fi
+if [[ -n $SSH_KEY ]] ; then apt install -y ssh && echo $SSH_KEY > /root/.ssh/authorized_keys; chmod 0600 /root/.ssh/authorized_keys && service ssh restart; fi
 # Часть 2 Переменные
-
 if [[ -n $RPC ]]
 then
-	if [[ -z $CHAIN ]]
-	then
-		CHAIN=`curl -s $RPC/status | jq -r .result.node_info.network`
-	fi
-	if [[ -z $BINARY_VERSION ]]
-	then
-		BINARY_VERSION=`curl -s $RPC/abci_info | jq -r .result.response.version`
-	fi
+	if [[ -z $CHAIN ]] ; then CHAIN=`curl -s $RPC/status | jq -r .result.node_info.network`; fi
+	if [[ -z $BINARY_VERSION ]] ; then BINARY_VERSION=`curl -s $RPC/abci_info | jq -r .result.response.version` ; fi
 fi
-echo 'export MONIKER='${MONIKER} >> /root/.bashrc
-echo 'export BINARY_VERSION='${BINARY_VERSION} >> /root/.bashrc
-echo 'export CHAIN='${CHAIN} >> /root/.bashrc
-echo 'export RPC='${RPC} >> /root/.bashrc
-echo 'export GENESIS='${GENESIS} >> /root/.bashrc
+echo 'export MONIKER='${MONIKER} >> /root/.bashrc ; echo 'export BINARY_VERSION='${BINARY_VERSION} >> /root/.bashrc ; echo 'export CHAIN='${CHAIN} >> /root/.bashrc ; echo 'export RPC='${RPC} >> /root/.bashrc ; echo 'export GENESIS='${GENESIS} >> /root/.bashrc
 # Часть 3 Компиляция
 if [[ -n $BINARY_LINK ]]
 then
-	if echo $BINARY_LINK | grep tar
-	then
-		wget -O /tmp/$BINARY.tar.gz $BINARY_LINK && tar -xvf /tmp/$BINARY.tar.gz -C /usr/bin/
-	else
-		wget -O /usr/bin/$BINARY $BINARY_LINK
-	fi
+	if echo $BINARY_LINK | grep tar ; then wget -O /tmp/$BINARY.tar.gz $BINARY_LINK && tar -xvf /tmp/$BINARY.tar.gz -C /usr/bin/ ; else wget -O /usr/bin/$BINARY $BINARY_LINK ; fi
 else
-	GIT_FOLDER=`basename $GITHUB_REPOSITORY | sed "s/.git//"`
-	git clone $GITHUB_REPOSITORY && cd $GIT_FOLDER && git checkout $BINARY_VERSION 
-	make build
-	make install
+	GIT_FOLDER=`basename $GITHUB_REPOSITORY | sed "s/.git//"` && git clone $GITHUB_REPOSITORY && cd $GIT_FOLDER && git checkout $BINARY_VERSION 
+	make build && make install
 	BINARY=`ls /root/go/bin`
-	if [[ -z $BINARY ]]
-	then
-		BINARY=`ls /root/$GIT_FOLDER/build/` && cp /root/$GIT_FOLDER/build/$BINARY /usr/bin/$BINARY
-	else
-		cp /root/go/bin/$BINARY /usr/bin/$BINARY
-	fi
+	if [[ -z $BINARY ]] ; then BINARY=`ls /root/$GIT_FOLDER/build/` && cp /root/$GIT_FOLDER/build/$BINARY /usr/bin/$BINARY ; else cp /root/go/bin/$BINARY /usr/bin/$BINARY ; fi
 fi
-sleep 3
-chmod +x /usr/bin/$BINARY
-echo $BINARY && echo 'export BINARY='${BINARY} >> /root/.bashrc && $BINARY version
-
+sleep 1
+chmod +x /usr/bin/$BINARY && echo $BINARY && echo 'export BINARY='${BINARY} >> /root/.bashrc && $BINARY version
 # Часть 4 Конфигурирование
-$BINARY init "$MONIKER" --chain-id $CHAIN  && sleep 5 
+$BINARY init "$MONIKER" --chain-id $CHAIN  && sleep 1
 
-if [[ -z $FOLDER ]]
-then
-FOLDER=.`echo $BINARY | sed "s/d$//"`
-fi
+if [[ -z $FOLDER ]] ; then FOLDER=.`echo $BINARY | sed "s/d$//"` ; fi
 
 if [[ -n ${RPC} ]] && [[ -z ${GENESIS} ]]
 then 
-	rm /root/$FOLDER/config/genesis.json
-	curl -s $RPC/genesis | jq .result.genesis >> /root/$FOLDER/config/genesis.json
-	if [[ -z $DENOM ]]
-	then
-		DENOM=`curl -s $RPC/genesis | grep denom -m 1 | tr -d \"\, | sed "s/denom://" | tr -d \ ` && 	echo 'export DENOM='${DENOM} >> /root/.bashrc
-	fi
+	rm /root/$FOLDER/config/genesis.json &&	curl -s $RPC/genesis | jq .result.genesis >> /root/$FOLDER/config/genesis.json
+	if [[ -z $DENOM ]] ; then DENOM=`curl -s $RPC/genesis | grep denom -m 1 | tr -d \"\, | sed "s/denom://" | tr -d \ ` && 	echo 'export DENOM='${DENOM} >> /root/.bashrc ;	fi
 fi
 if [[ -n $GENESIS ]]
 then	
@@ -97,33 +47,21 @@ then
 		rm /root/$FOLDER/config/genesis.json && mkdir /tmp/genesis/
 		wget -O /tmp/genesis.tar.gz $GENESIS && tar -C /tmp/genesis/ -xf /tmp/genesis.tar.gz
 		rm /tmp/genesis.tar.gz && mv /tmp/genesis/`ls /tmp/genesis/` /root/$FOLDER/config/genesis.json		
-		if [[ -z $DENOM ]]
-		then
-			DENOM=`curl -s $RPC/genesis | grep denom -m 1 | tr -d \"\, | sed "s/denom://" | tr -d \ ` && echo 'export DENOM='${DENOM} >> /root/.bashrc
-		fi
+		if [[ -z $DENOM ]] ; then DENOM=`curl -s $RPC/genesis | grep denom -m 1 | tr -d \"\, | sed "s/denom://" | tr -d \ ` && echo 'export DENOM='${DENOM} >> /root/.bashrc ; fi
 	else
 		rm /root/$FOLDER/config/genesis.json && wget -O /root/$FOLDER/config/genesis.json $GENESIS
-		if [[ -z $DENOM ]]
-		then
-			DENOM=`curl -s $RPC/genesis | grep denom -m 1 | tr -d \"\, | sed "s/denom://" | tr -d \ ` && echo 'export DENOM='${DENOM} >> /root/.bashrc
-		fi
+		if [[ -z $DENOM ]] ; then DENOM=`curl -s $RPC/genesis | grep denom -m 1 | tr -d \"\, | sed "s/denom://" | tr -d \ ` && echo 'export DENOM='${DENOM} >> /root/.bashrc ; fi
 	fi
 fi
-echo $DENOM
-sleep 5
+echo $DENOM && sleep 1
 if [[ -n $SNAPSHOT ]]
 then
-echo == Download snapshot ==
-echo = Скачивание снепшота =
-cp /root/$FOLDER/data/priv_validator_state.json /root/$FOLDER/priv_validator_state.json.backup 
-$BINARY tendermint unsafe-reset-all --keep-addr-book 
+cp /root/$FOLDER/data/priv_validator_state.json /root/$FOLDER/priv_validator_state.json.backup && $BINARY tendermint unsafe-reset-all --keep-addr-book 
 SIZE=`wget --spider $SNAPSHOT 2>&1 | awk '/Length/ {print $2}'`
 echo == Download snapshot ==
 (wget -nv -O - $SNAPSHOT | pv -petrafb -s $SIZE -i 5 | lz4 -dc - | tar -xf - -C /root/$FOLDER) 2>&1 | stdbuf -o0 tr '\r' '\n'
 echo == Complited ==
-echo == Завершено ==
-mv /root/$FOLDER/priv_validator_state.json.backup /root/$FOLDER/data/priv_validator_state.json
-STATE_SYNC=off
+mv /root/$FOLDER/priv_validator_state.json.backup /root/$FOLDER/data/priv_validator_state.json && STATE_SYNC=off
 fi
 if [[ -n ${RPC} ]] && [[  -z "$PEERS" ]]
 then
@@ -151,14 +89,8 @@ sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025$DENOM\"/
 sed -i.bak -e "s/^double_sign_check_height *=.*/double_sign_check_height = 15/;" /root/$FOLDER/config/config.toml
 sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/;" /root/$FOLDER/config/config.toml
 sed -i.bak -e "s|^persistent_peers *=.*|persistent_peers = \"$PEERS\"|;" /root/$FOLDER/config/config.toml
-if [[ -z $DISABLE_RPC ]]
-then
-sed -i.bak -e "s_"tcp://127.0.0.1:26657"_"tcp://0.0.0.0:26657"_;" /root/$FOLDER/config/config.toml
-fi
-if [[ -z $KEEP_RECENT ]] || [[ -z $INTERVAL ]]
-then
-KEEP_RECENT=1000 && INTERVAL=10
-fi
+if [[ -z $DISABLE_RPC ]] ; then sed -i.bak -e "s_"tcp://127.0.0.1:26657"_"tcp://0.0.0.0:26657"_;" /root/$FOLDER/config/config.toml ; fi
+if [[ -z $KEEP_RECENT ]] || [[ -z $INTERVAL ]] ; then KEEP_RECENT=1000 && INTERVAL=10 ; fi
 if [[ -z $PRUNING ]]
 then
   sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" /root/$FOLDER/config/app.toml && \
@@ -169,21 +101,14 @@ else
   sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$KEEP_RECENT\"/" /root/$FOLDER/config/app.toml && \
   sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$INTERVAL\"/" /root/$FOLDER/config/app.toml
 fi
-if [[ -z $INDEXER ]]
-then
-INDEXER=kv
-fi
+if [[ -z $INDEXER ]] ; then INDEXER=kv ; fi
 sed -i -e "s/^indexer *=.*/indexer = \"$INDEXER\"/" /root/$FOLDER/config/config.toml
-if [[ -z $SNAPSHOT_INTERVAL ]]
-then
-SNAPSHOT_INTERVAL="2000" 
-fi
+if [[ -z $SNAPSHOT_INTERVAL ]] ; then SNAPSHOT_INTERVAL="2000" ; fi
 sed -i.bak -e "s/^snapshot-interval *=.*/snapshot-interval = \"$SNAPSHOT_INTERVAL\"/" /root/$FOLDER/config/app.toml
 
 # ====================RPC======================
 if [[ -n ${RPC} ]] && [[ -z $STATE_SYNC ]]
-then
-	
+then	
 	LATEST_HEIGHT=`curl -s $RPC/block | jq -r .result.block.header.height` 
 	BLOCK_HEIGHT=$((LATEST_HEIGHT - 2000)) && BLOCK_HEIGHT=`echo $BLOCK_HEIGHT | sed "s/...$/000/"`
 	TRUST_HASH=$(curl -s "$RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
@@ -196,10 +121,7 @@ then
 fi
 #================================================
 
-if [[ -n ${VALIDATOR_KEY_JSON_BASE64} ]]
-then
-echo $VALIDATOR_KEY_JSON_BASE64 | base64 -d > /root/$FOLDER/config/priv_validator_key.json
-fi
+if [[ -n ${VALIDATOR_KEY_JSON_BASE64} ]] ; then echo $VALIDATOR_KEY_JSON_BASE64 | base64 -d > /root/$FOLDER/config/priv_validator_key.json ; fi
 
 # Часть 5 Запуск
 if [[ -n ${RPC} ]]
@@ -235,8 +157,4 @@ EOF
 chmod +x /root/$BINARY/log/run /root/$BINARY/run 
 ln -s /root/$BINARY /etc/service && ln -s /tmp/log/current /LOG
 sleep 20
-for ((;;))
-  do    
-    tail -100 /LOG | grep -iv peer && sleep 10m
-  done
-fi
+while true ; do tail -100 /LOG | grep -iv peer && sleep 10m ; done
