b740140ccd7be28fba6b95cbdf77372cc4ba21e0
Dimokus <Dimokus@mail.ru>
2023-06-14 12:02:03 +0300

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,4 +1,4 @@
-#Running this script: nohup sh -c 'curl -s https://raw.githubusercontent.com/DecloudNodesLab/CodeBase/main/scripts/update.sh | BINARY_LINK= UPDATE_BLOCK_NUMBER= BINARY= bash' & tail -f /root/nohup.out
+#Running this script: nohup sh -c 'curl -s https://raw.githubusercontent.com/DecloudNodesLab/CodeBase/main/scripts/update.sh | BINARY_LINK= UPDATE_BLOCK_NUMBER= BINARY= bash' & sleep 1 ; tail -f /root/nohup.out
 
 #!/bin/bash
 if [[ -z $BINARY_LINK ]] || [{ -z $UPDATE_BLOCK_NUMBER ]] || [[ -z $BINARY ]]
